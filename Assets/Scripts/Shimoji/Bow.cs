f175aeed7d6599c6dca595455655ad170907dd75
ShimojiHiroto <handoballsky@gmail.com>
2021-08-14 13:45:49 +0900

    下地追加しました
    追加しました
    マージお願いします。

---
+++ Assets/Scripts/Shimoji/Bow.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class Bow : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(SearchAndShot());
+    }
+
+    private IEnumerator SearchAndShot()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(1.0f);
+            //TODO ここで敵を探して、矢を撃つ
+            var collider = Physics2D.OverlapCircle(transform.position, 2.0f, LayerMask.GetMask("Enemy"));
+            if(collider != null)
+            {
+                Bowtransform.rotation = Quaternion.FromToRotation(Vector3.right, collider.transform.position - transform.position);
+                var arrow = Instantiate(arrowPrefab, transform.position, transform.rotation);
+                arrow.targetEnemy = collider.GetComponent<Enemy>();
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Shimoji/ShotRange.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShotRange : MonoBehaviour
+{
+    public Player player;
+    public SpriteRenderer rangeRenderer;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (player.selectBow == null)
+        {
+            rangeRenderer.enabled = false;
+        }
+        else
+        {
+            rangeRenderer.enabled = true;
+            rangeRenderer.transform.localScale = new Vector3(2, 2) * player.selectBow.ShotRange;
+            rangeRenderer.transform.position = player.selectBow.transform.position;
+        }
+    }
+}
