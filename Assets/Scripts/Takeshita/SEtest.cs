dff455ea89829126befd3997a08ba308aa093c69
Chi-ni <dokoitsu0506@gmial.com>
2021-08-14 22:38:20 +0900

    SoundEffectManagerのプレハブを作成しました

---
+++ Assets/Scripts/Takeshita/SEtest.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SEtest : MonoBehaviour
+{
+    GameObject seManager;
+    SoundEffectManager se;
+
+    GameObject bgmManager;
+
+    string input;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        seManager = GameObject.Find("SoundEffectManager");
+        se = seManager.GetComponent<SoundEffectManager>();
+
+        bgmManager = GameObject.Find("bgmManager");
+        bgmManager.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        input = Input.inputString;
+
+        switch(input)
+        {
+            case ("1"):
+                se.DefeatEnemySE();
+                Debug.Log("Defeat Enemy");
+                break;
+            case ("2"):
+                se.GameOverSE();
+                Debug.Log("Game Over");
+                break;
+            case ("3"):
+                se.MyDamageSE();
+                Debug.Log("Damaged!");
+                break;
+            case ("4"):
+                se.SetBowSE();
+                Debug.Log("Set a Bow");
+                break;
+            case ("5"):
+                se.ShotArrowSE();
+                Debug.Log("Shoot!");
+                break;
+            case ("6"):
+                se.SpawnEnemySE();
+                Debug.Log("Spawn an Enemy");
+                break;
+            case ("7"):
+                se.StartGameSE();
+                Debug.Log("Game Start!");
+                break;
+            case ("8"):
+                se.StartWaveSE();
+                Debug.Log("Wave Start");
+                break;
+            default:
+                break;
+        }
+    }
+}
---
+++ Assets/Scripts/Takeshita/SoundEffectManager.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// SEの再生を制御するスクリプトです。
+/// 鳴らしたいタイミングで関数を呼ぶことでSEを鳴らすことができます。
+/// </summary>
+
+public class SoundEffectManager : MonoBehaviour
+{
+    AudioSource se;
+
+    [SerializeField] AudioClip defeatEnemy;
+    [SerializeField] AudioClip gameOvar;
+    [SerializeField] AudioClip myDamage;
+    [SerializeField] AudioClip setBow;
+    [SerializeField] AudioClip shotArrow;
+    [SerializeField] AudioClip spawnEnemy;
+    [SerializeField] AudioClip startGame;
+    [SerializeField] AudioClip startWave;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        DontDestroyOnLoad(this.gameObject);
+        se = GetComponent<AudioSource>();
+    }
+
+    public void DefeatEnemySE()
+    {
+        se.PlayOneShot(defeatEnemy);
+    }
+
+    public void GameOverSE()
+    {
+        se.PlayOneShot(gameOvar);
+    }
+
+    public void MyDamageSE()
+    {
+        se.PlayOneShot(myDamage);
+    }
+
+    public void SetBowSE()
+    {
+        se.PlayOneShot(setBow);
+    }
+
+    public void ShotArrowSE()
+    {
+        se.PlayOneShot(shotArrow);
+    }
+
+    public void SpawnEnemySE()
+    {
+        se.PlayOneShot(spawnEnemy);
+    }
+
+    public void StartGameSE()
+    {
+        se.PlayOneShot(startGame);
+    }
+
+    public void StartWaveSE()
+    {
+        se.PlayOneShot(startWave);
+    }
+}
