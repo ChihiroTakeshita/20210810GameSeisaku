3f455f165ed0095b973146b7ad4d86891b0fcf32
Chi-ni <dokoitsu0506@gmial.com>
2021-08-14 19:15:14 +0900

    bgmManagerプレハブでシーンごとにbgmを切り替えることができるはずです。

---
+++ Assets/Scripts/Takeshita/SoundManager.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+/// <summary>
+/// シーンによってBGMを変えるスクリプトです
+/// </summary>
+
+public class SoundManager : MonoBehaviour
+{
+    [SerializeField] public AudioSource prologue;
+    [SerializeField] public AudioSource game;
+    [SerializeField] public AudioSource gameClear;
+    [SerializeField] public AudioSource gamOver;
+
+    private string beforeScene = "Prologue";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        DontDestroyOnLoad(this.gameObject);
+        DontDestroyOnLoad(prologue.gameObject);
+        DontDestroyOnLoad(game.gameObject);
+        DontDestroyOnLoad(gameClear.gameObject);
+        DontDestroyOnLoad(gamOver.gameObject);
+
+        SceneManager.activeSceneChanged += OnActiveSceneChanged;
+    }
+
+    /// <summary>
+    /// シーン遷移時にシーンの名前を取得して、当てはまるシーンのBGMを流します
+    /// </summary>
+    /// <param name="prevScene"></param>
+    /// <param name="nextScene"></param>
+
+    void OnActiveSceneChanged(Scene prevScene, Scene nextScene)
+    {
+        if(beforeScene == "Prologue" && nextScene.name == "Title")
+        {
+            prologue.Stop();
+            game.Play();
+        }
+
+        if (beforeScene == "Game" && nextScene.name == "GameClear")
+        {
+            game.Stop();
+            gameClear.Play();
+        }
+
+        if (beforeScene == "Game" && nextScene.name == "GameOver")
+        {
+            game.Stop();
+            gamOver.Play();
+        }
+    }
+}
