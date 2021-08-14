9ecefecc03cf156888dceff5d60080648104f4e6
Chi-ni <dokoitsu0506@gmial.com>
2021-08-14 14:11:50 +0900

    Stageプレハブを修正しました

---
+++ Assets/Scripts/Takeshita/EnemyManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyManager : MonoBehaviour
+{
+    public Wave[] waves;
+    public int wave;
+    public float time;
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void CreateEnemy()
+    {
+        foreach (var pattern in waves[wave].patterns)
+        {
+            if (pattern.time <= time)
+            {
+                var enemy = Instantiate(pattern.enemy, new Vector3(-1000, -1000), Quaternion.identity);
+                enemy.route = pattern.route;
+            }
+        }
+
+        waves[wave].patterns.RemoveAll(pattern => pattern.time <= time);
+    }
+}
+
+[SerializeField]
+public class Wave
+{
+    public List<EnemyPattern> patterns;
+}
+
+[SerializeField]
+public class EnemyPattern
+{
+    public float time;
+    public Enemy enemy;
+    public Route route;
+}
