5163c1c8318e10b625b08c7a12b9e08899e96fcc
TsuguhikoOkada <tsugu.pc.academy@gmail.com>
2021-08-15 23:57:02 +0900

    画面遷移のスクリプトとスクリプトテストのための仮シーンを追加
    画面遷移のスクリプトはSpaceキーを押したらシーンを切り替えることができます。

---
+++ Assets/Scripts/Tsuguhiko/Scene_Changer.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+/// <summary>
+///   画面遷移のスクリプト（spaceキーを押したら遷移する)
+/// </summary>
+
+
+public class Scene_Changer : MonoBehaviour
+{
+    [SerializeField] string sceneName; // シーン名をInspectorでセッティング
+
+    [SerializeField] AudioClip PushButton_SE;
+     AudioSource audioSource;
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Componentを取得
+        audioSource = GetComponent<AudioSource>();
+
+        StartCoroutine(Checking(()=>
+            {
+            Debug.Log("End");
+             }));
+    }
+
+    public delegate void functionType();
+
+    private IEnumerator Checking (functionType callback)
+    {
+        while (true)
+        {
+            yield return new WaitForFixedUpdate();
+
+            if (!audioSource.isPlaying)
+            {
+                callback();
+                break;
+            }
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) ) // spaceキーが押されたら
+        {
+            audioSource.PlayOneShot(PushButton_SE); // Buttonの効果音を鳴らす
+            StartCoroutine(ChangeScene(sceneName, 1.0f));
+        }
+    }
+
+    IEnumerator ChangeScene(string name, float seconds)
+    {
+        yield return new WaitForSeconds(seconds);
+        SceneManager.LoadScene(name); // Inspector上で遷移するSceneの名前をセッティングした場所へ飛ぶ
+    }
+}
